edeb9c1eba1428e9e62c6db25144c2e6ad4da10e
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2026-06-24 23:23:26 +0200

    Make Windows 10 start provider code-only

--- src/WindBar.App/Views/StartMenuTen.xaml.cs
+++ src/WindBar.App/Views/StartMenuTen.xaml.cs
@@ -1,19 +1,24 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using WindBar.Core;
 
 namespace WindBar.App.Views
 {
-    public partial class StartMenuTen : System.Windows.Controls.UserControl, IStartMenuProvider
+    public partial class StartMenuTen : UserControl, IStartMenuProvider
     {
         public StartMenuTen()
         {
-            InitializeComponent();
+            Content = new TextBlock
+            {
+                Text = "Windows 10 style Start, re-imagined for Windows 11",
+                Foreground = Brushes.White,
+                FontSize = 24,
+                Margin = new Thickness(24)
+            };
         }
 
         public string DisplayName => "Windows 10 Start";
-
-        public object CreateView()
-        {
-            return this;
-        }
+        public object CreateView() => this;
     }
 }
